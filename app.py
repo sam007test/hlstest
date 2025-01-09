67d854668014ee296677271756b0737feb7de23d
sam007test <167562267+sam007test@users.noreply.github.com>
2025-01-09 20:30:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,5 +144,5 @@ def serve_stream(filename):
         return str(e), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = 5000
     app.run(host='0.0.0.0', port=port)
